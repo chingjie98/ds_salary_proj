7db0fc22c8b08ae140b910463ca95755656cb5ac
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2021-01-29 21:50:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,7 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 import matplotlib.pyplot as plt
-plt.scatter(X_train, y_train, color = 'red')
-
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.scatter(X_test, y_test, color = 'red')
+plt.scatter(X_test, y_test, color = 'black')
 plt.plot(X_train, regressor.predict(X_train), color='blue')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
